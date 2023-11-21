8b2c75ffdc88f0b823595a1344f6365f07a14e24
zhuravlevts <the.zhts@yandex.ru>
2023-11-21 06:49:45 +0300

    Change author model

--- homeLibrary/Models/Author.cs
+++ homeLibrary/Models/Author.cs
@@ -4,9 +4,7 @@ namespace homeLibrary.Models
 	public class Author
 	{
 		public int Id { get; set; }
-		public string FirstName { get; set; }
-		public string? MiddleName { get; set; }
-		public string LastName { get; set; }
+		public string FMLName { get; set; }
 		public ICollection<BookAuthor> BookAuthor { get; set; }
         public DateTime Created { get; set; }
     }
